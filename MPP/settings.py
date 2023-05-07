4b78346ece7a4fdf2da14568b836bb67e1c38e25
Robert Haranguș <robert.harangus@gmail.com>
2023-05-07 12:04:57 +0300

    #23: Postgre Server

--- MPP/settings.py
+++ MPP/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'MPP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '34.118.104.246',
+        'USER': 'FrontUser',
+        'PASSWORD': '123',
+        'NAME': 'Movies',
     }
 }
 
