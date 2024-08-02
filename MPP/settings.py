38b0813306ccd36522c8425c538aacb153a6e1d9
Haranguș Robert-Adrian <robert.harangus@gmail.com>
2024-08-02 22:27:20 +0300

    Update settings.py

--- MPP/settings.py
+++ MPP/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'MPP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '34.118.104.246',
+        'HOST': '34.*******.246',
         'USER': 'FrontUser',
         'PASSWORD': '123',
         'NAME': 'Movies',
